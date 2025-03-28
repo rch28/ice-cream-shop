946b810ff4e3b91c81fbf1dc64b27bb8c72276a4
Raju Chhetri <rajuchhetry11@gmail.com>
2025-03-28 23:25:28 +0545

    fix: Featured card title

--- src/components/Common/FeaturedCard.jsx
+++ src/components/Common/FeaturedCard.jsx
@@ -1,8 +1,10 @@
+import { ArrowRight } from "lucide-react";
+import Link from "next/link";
 import React from "react";
 
 const FeaturedCard = ({ image, name, price }) => {
   return (
-    <div className="group cursor-pointer ">
+    <div className="group cursor-pointer  relative">
       <div className="aspect-square rounded-2xl shadow shadow-gray-200 drop-shadow-2xl overflow-hidden mb-4">
         <img
           src={image}
@@ -10,8 +12,10 @@ const FeaturedCard = ({ image, name, price }) => {
           className="w-full h-full object-cover group-hover:scale-110 transition-transform duration-300"
         />
       </div>
-      <h3 className="text-lg font-semibold mb-2">{name}</h3>
-      <p className="text-pink-600 font-semibold">{price}</p>
+      <div className="flex justify-between items-center">
+        <h3 className="text-lg black font-semibold ">{name}</h3>
+        <p className="text-secondary font-bold">{price}</p>
+      </div>
     </div>
   );
 };
